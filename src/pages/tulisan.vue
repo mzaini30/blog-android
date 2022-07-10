2bc4116df71e44c11229b0e1c8944347abca5b2d
Zen <muhzaini30@gmail.com>
2022-07-11 07:36:18 +0800

    sudah

--- src/pages/tulisan.vue
+++ src/pages/tulisan.vue
@@ -1,5 +1,10 @@
 <script setup="" lang="ts">
 import { onMounted } from "vue";
+import tulisan from "../tulisan";
+import { acak } from "kumpulan-tools";
+
+const tulisanLainnya = acak(tulisan).slice(0, 5);
+
 onMounted(() => {
   const adaH1 = document.querySelector("h1");
   if (adaH1) {
@@ -11,5 +16,11 @@ onMounted(() => {
 <template>
   <div class="tulisan prose mx-auto p-3">
     <router-view></router-view>
+    <h2>Tulisan Lainnya</h2>
+    <ul>
+      <li v-for="x in tulisanLainnya">
+        <router-link :to="`/tulisan/${x.slug}`">{{ x.judul }}</router-link>
+      </li>
+    </ul>
   </div>
 </template>
