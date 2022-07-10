2a4a858935289ea3ebbfc9bfc10fac7530cdb35b
Zen <muhzaini30@gmail.com>
2022-07-11 07:40:00 +0800

    sudah

--- src/pages/tulisan.vue
+++ src/pages/tulisan.vue
@@ -1,7 +1,26 @@
 <script setup="" lang="ts">
 import { onMounted } from "vue";
 import tulisan from "../tulisan";
-import { acak } from "kumpulan-tools";
+
+function acak(array: Array<any>): Array<any> {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 const tulisanLainnya = acak(tulisan).slice(0, 5);
 
